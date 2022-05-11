640c2e54d3030a97a5aab12311290dedda824a61
Renato Passarelli <renatoguedespassarelli@gmail.com>
2022-05-11 10:13:03 -0300

    importando?

--- Jogo.py
+++ Jogo.py
@@ -1,26 +1,2 @@
-import math
-from Lista import DADOS
-from Lista import EARTH_RADIUS
-import sorteando_países
-import Normalizando_base_de_dados
-import Adicionando_em_uma_Lista_Ordenada
-import Distância_de_Haversine
-import Esta_na_lista
-import sorteia_com_restrição
 
-print("=======================================")
-print("|                                     |")
-print("|    Bem vindo ao jogo dos países     |")
-print("|                                     |")
-print("|    ==== Design de Software ====     |")
-print("|                                     |")
-print("|     Feito por Renato.P e Felipe.F   |")
-print("=======================================")
-print("                                       ")
-print("Comandos Básicos:                      ")
-print("dica         ----   entra no mercado de dicas")
-print("Desisto      ----   desiste da rodada")
-print("Inventário   ----   exibe sua posição")
-print("                                       ")
-print("                                       ")
-print("Um país foi escolhido, tente adivinhar qual é!!")
+
