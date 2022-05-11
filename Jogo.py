23e6406e33faacf5fcb9267a76653233d3be97dc
Felipe Fuchs <felipelf3@al.insper.edu.br>
2022-05-11 09:54:07 -0300

    Jogo interface

---
+++ Jogo.py
@@ -0,0 +1,19 @@
+import math
+
+
+print("=======================================")
+print("|                                     |")
+print("|    Bem vindo ao jogo dos países     |")
+print("|                                     |")
+print("|    ==== Design de Software ====     |")
+print("|                                     |")
+print("|     Feito por Renato.P e Felipe.F   |")
+print("=======================================")
+print("                                       ")
+print("Comandos Básicos:                      ")
+print("dica         ----   entra no mercado de dicas")
+print("Desisto      ----   desiste da rodada")
+print("Inventário   ----   exibe sua posição")
+print("                                       ")
+print("                                       ")
+print("Um país foi escolhido, tente adivinhar qual é!!")
