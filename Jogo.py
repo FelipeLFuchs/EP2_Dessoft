cab8f9e32db84d2ce54946c4be1fade1402788e6
Renato Passarelli <renatoguedespassarelli@gmail.com>
2022-05-13 12:14:05 -0300

    brasil coloridão e sistema de desistir

--- Jogo.py
+++ Jogo.py
@@ -8,16 +8,17 @@ from Adicionando_em_uma_Lista_Ordenada import adiciona_em_ordem
 from Distância_de_Haversine import haversine
 import Esta_na_lista
 import sorteia_com_restrição
-class bcolors:    
+class bcolors:   
     RED = "\033[1;91m" 
     BLUE = "\033[1;34m" 
     CYAN = "\033[1;36m" 
     GREEN = "\033[1;92m"
-    GRAY = "\033[1;49m"
-    YELLOW = "\033[1;33m"
+    GREENBR="\033[1;32m"
+    GRAY = "\033[1;90m"
+    YELLOW = "\033[1;93m"
+    YELLOWBR = "\033[1;33m"
     MAGENTA = "\033[1;95m"
     RESET="\033[0m"
-
 print("=======================================")
 print("|                                     |")
 print("|    Bem vindo ao jogo dos países     |")
@@ -35,7 +36,7 @@ print("Lista   ----   exibe todos os países possíveis")
 final=0
 while final==0:       #iniciando o jogo
     tentativas=0      #parâmetros
-    faltam=0
+    faltam=20
     print("                                       ")
     print("                                       ")
     print("Um país foi escolhido, tente adivinhar qual é!!")  
@@ -68,6 +69,7 @@ while final==0:       #iniciando o jogo
 
 
     while tentativas<20:           #TENTATIVAS
+        print("")
         chuten=input("Qual seu palpite  ")
         chute=chuten.lower()
         
@@ -76,17 +78,65 @@ while final==0:       #iniciando o jogo
 
 
 
-        if chute in l:                            #CHUTA EM PAÍSES
-            if chute != sorteado:
-                latitudech=Normalizada[chute]["geo"]["latitude"]
-                longitudech=Normalizada[chute]["geo"]["longitude"]
-                distancia= (haversine(EARTH_RADIUS, latitudesort, longitudesort, latitudech, longitudech))
-                inteiro=int(distancia)+1
-                if (inteiro-distancia)<0.50:
-                    distanciar = (int(distancia)+1)
-                else:
-                    distanciar = (int(distancia))
-                lista = (adiciona_em_ordem(chute, distanciar,lista))
+        if chute in l and chute != sorteado:                            #CHUTA EM PAÍSES
+            latitudech=Normalizada[chute]["geo"]["latitude"]
+            longitudech=Normalizada[chute]["geo"]["longitude"]
+            distancia= (haversine(EARTH_RADIUS, latitudesort, longitudesort, latitudech, longitudech))
+            inteiro=int(distancia)+1
+            if (inteiro-distancia)<0.50:
+                distanciar = (int(distancia)+1)
+            else:
+                distanciar = (int(distancia))
+            lista = (adiciona_em_ordem(chute, distanciar,lista))
+
+
+
+            y=0                                                     #DISTÂNCIA E CORES
+            indidica=0
+            print("Distâncias:")
+            print("")
+            while y < len(lista):
+                if lista [y][0]!= "brasil":
+                    if lista[y][1] <=500:
+                        print((bcolors.CYAN+'{0} Km -> {1}'+bcolors.RESET).format(lista[y][1],lista[y][0]))
+                    if lista[y][1] > 500 and lista[y][1] <= 1000:
+                        print((bcolors.GREEN +'{0} Km -> {1}'+bcolors.RESET).format(lista[y][1],lista[y][0]))
+                    if lista[y][1] > 1000 and lista[y][1] <= 2000:
+                        print((bcolors.YELLOW +'{0:.3f} Km -> {1}'+bcolors.RESET).format(lista[y][1]/1000,lista[y][0]))
+                    if lista[y][1] > 2000 and lista[y][1] <= 5000:
+                        print((bcolors.MAGENTA +'{0:.3f} Km -> {1}'+bcolors.RESET).format(lista[y][1]/1000,lista[y][0]))
+                    if lista[y][1] > 5000 and lista[y][1] <= 10000:
+                        print((bcolors.RED +'{0:.3f} Km -> {1}'+bcolors.RESET).format(lista[y][1]/1000,lista[y][0]))
+                    if lista[y][1] > 10000:
+                        print((bcolors.GRAY +'{0:.3f} Km -> {1}'+bcolors.RESET).format(lista[y][1]/1000,lista[y][0]))
+                    
+                    y +=1
+                if lista [y][0]== "brasil":                             #Brasillllllll
+                    if lista[y][1] <=500:
+                        print((bcolors.CYAN+"{0} Km ->"+bcolors.YELLOWBR+"{1}"+bcolors.BLUE+"{2}"+bcolors.GREENBR+"{3}"+bcolors.RESET).format(lista[y][1],lista[y][0][0:2],lista[y][0][2:4],lista[y][0][4:6]))
+                    if lista[y][1] > 500 and lista[y][1] <= 1000:
+                        print((bcolors.GREEN+"{0} Km ->"+bcolors.YELLOWBR+"{1}"+bcolors.BLUE+"{2}"+bcolors.GREENBR+"{3}"+bcolors.RESET).format(lista[y][1],lista[y][0][0:2],lista[y][0][2:4],lista[y][0][4:6]))
+                    if lista[y][1] > 1000 and lista[y][1] <= 2000:
+                        print((bcolors.YELLOW+"{0:.3f} Km ->"+bcolors.YELLOWBR+"{1}"+bcolors.BLUE+"{2}"+bcolors.GREENBR+"{3}"+bcolors.RESET).format(lista[y][1]/1000,lista[y][0][0:2],lista[y][0][2:4],lista[y][0][4:6]))
+                    if lista[y][1] > 2000 and lista[y][1] <= 5000:
+                        print((bcolors.MAGENTA+"{0:.3f} Km ->"+bcolors.YELLOWBR+"{1}"+bcolors.BLUE+"{2}"+bcolors.GREENBR+"{3}"+bcolors.RESET).format(lista[y][1]/1000,lista[y][0][0:2],lista[y][0][2:4],lista[y][0][4:6]))
+                    if lista[y][1] > 5000 and lista[y][1] <= 10000:
+                        print((bcolors.RED+"{0:.3f} Km ->"+bcolors.YELLOWBR+"{1}"+bcolors.BLUE+"{2}"+bcolors.GREENBR+"{3}"+bcolors.RESET).format(lista[y][1]/1000,lista[y][0][0:2],lista[y][0][2:4],lista[y][0][4:6]))
+                    if lista[y][1] > 10000:
+                        print((bcolors.GRAY+"{0:.3f} Km ->"+bcolors.YELLOWBR+"{1}"+bcolors.BLUE+"{2}"+bcolors.GREENBR+"{3}"+bcolors.RESET).format(lista[y][1]/1000,lista[y][0][0:2],lista[y][0][2:4],lista[y][0][4:6]))
+                    
+                    y +=1
+            tentativas+=1
+            print("")
+            print("Dicas:")
+            while indidica<len(listdicas):
+                print(listdicas[indidica])
+                indidica+=1
+            print("")
+            faltam=20-tentativas
+            print("Você tem {0} tentativa(s)".format(faltam))        
+            
+
 
 
         if chute == 'dica':          #DICA
@@ -141,63 +191,59 @@ while final==0:       #iniciando o jogo
 
 
 
-        y=0
-        if chute!= sorteado:          #DISTÂNCIA E CORES
-            bcolors.RED+'ola'+bcolors.RESET
-            indidica=0
-            print("Distâncias:")
-            print("")
-            while y < len(lista):
-                
-                if lista[y][1] <=500:
-                    print((bcolors.CYAN+'{0} Km -> {1}'+bcolors.RESET).format(lista[y][1],lista[y][0]))
-                if lista[y][1] > 500 and lista[y][1] <= 1000:
-                    print((bcolors.GREEN +'{0} Km -> {1}'+bcolors.RESET).format(lista[y][1],lista[y][0]))
-                if lista[y][1] > 1000 and lista[y][1] <= 2000:
-                    print((bcolors.YELLOW +'{0:.3f} Km -> {1}'+bcolors.RESET).format(lista[y][1]/1000,lista[y][0]))
-                if lista[y][1] > 2000 and lista[y][1] <= 5000:
-                    print((bcolors.MAGENTA +'{0:.3f} Km -> {1}'+bcolors.RESET).format(lista[y][1]/1000,lista[y][0]))
-                if lista[y][1] > 5000 and lista[y][1] <= 10000:
-                    print((bcolors.RED +'{0:.3f} Km -> {1}'+bcolors.RESET).format(lista[y][1]/1000,lista[y][0]))
-                if lista[y][1] > 10000:
-                    print((bcolors.GRAY +'{0:.3f} Km -> {1}'+bcolors.RESET).format(lista[y][1]/1000,lista[y][0]))
-                
-                y +=1
-            tentativas+=1
-            print("")
-            print("Dicas:")
-            while indidica<len(listdicas):
-                print(listdicas[indidica])
-                indidica+=1
-            print("")
-            faltam=20-tentativas
-            print("Você tem {0} tentativa(s)".format(faltam))
-
-
-
         if chute == sorteado:          #ACERTOU
             tentativas+=1
+            print("")
             print("***Parabéns meu consagrado, parece que temos um Sherlock Holmes aqui!!!***")
             print("você acertou após {0} tentativas".format(tentativas))
             acerto+=1
+            print("")
             b=input("Quer jogar novamente?[s|n]")
             if b=="n":
                 final+=1
                 tentativas=20
-            else:
-                final=0
-                tentativas=20
+                print("Até a próxima!!!!")
 
+            if b =="s":
+                final=0
+                tentativas=21
+
+
+
+        if chute=="desisto":    
+            print("")                               #desisto
+            desistir=input("tem certeza que vai desistir??[s|n]")
+            if desistir == "s":
+                tentativas=20
+                acerto+=1
+                print("")
+                print("Não aguentou a pressão??? O país era {0}".format(sorteado))
+                novamente=input("Quer jogar novamente?[s|n]")
+                print("")
+                print("Até a próxima!!!!")
+                final+=1
+            if desistir=="n":
+                print("")
+                print("Você ainda tem {0} tentativa(s)".format(faltam))
 
 
         if tentativas>=20 and acerto ==0:               #ERROU
+            print("")
             print("Dormiu nas aulas de geografia? O país escolhido era {0}".format(sorteado))
+            print("")
             b=input("Quer jogar novamente?[s|n]")
-            if b=="n":
-                final+=1
             if b=="s":
-                final=0
-                tentativas=0
+                tentativa=20
+            
+            if b=="n":
+                print("")
+                print("Até a próxima!!!!")
+                final+=1
+                    
+
+        if chute not in l and chute != "dica" and chute != "desisto" and chute != "lista":          #desconhecido
+            print("")
+            print("País não reconhecido, tente novamente")
+
 final=0
     
-#RED = "033 [1;31m" BLUE = "033 [1;34m" CYAN = "033 [1;36m" GREEN = "033 [0;32m" RESET = "033
